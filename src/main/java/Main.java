98f0d27a56c2f6c369aa303ce5039bd5a2e5106f
Eryk <eryk@Zipcoders-MacBook-Pro-2.local>
2022-10-26 16:53:04 -0400

    greets those named Alice or Bob only

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        Scanner enterName = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String userName = enterName.nextLine();
+        if(userName.equals("Alice") || userName.equals("Bob")){
+            System.out.println("Greetings " + userName);
+        }else{
+            System.out.println("You are not Alice or Bob!");
+        }
 
     }
 }
